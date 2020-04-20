06df026f14e7f8c8669a6b2ac07513a20ef3bc16
fallGamlet <fallgamlet@gmail.com>
2020-04-20 10:45:21 +0300

    change module name

--- customer/src/main/java/com/tracking/observer/MapFragment.kt
+++ customer/src/main/java/com/tracking/observer/MapFragment.kt
@@ -1,7 +1,9 @@
 package com.tracking.observer
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,6 +16,10 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var googleMap: GoogleMap
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.screen_map, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
